32fff7067e0d57124c4baf38613304bf8e3e823d
Michelle Scaramuzzo <michellescaramuzzo@Michelles-MacBook-Pro.local>
2025-01-05 22:17:42 -0600

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import './App.css';
+import { puppyList } from './data.js';
+import { useState } from 'react';
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
   return (
     <div className="App">
-      {/* XX */}
+      {/* Map over the puppies array and return a <p> tag for each puppy's name */}
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
